a24460df8bde8b9d07059b37603601f8b2a3ab19
spenshark <xzx01299@naver.com>
2024-02-08 02:38:20 +0900

    first commit

---
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -0,0 +1,39 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Slf4j
+@Controller
+public class ArticleController {
+    @Autowired // 스프링 부트가 미리 생성해 놓은 리포지터리 객체 주입(DI)
+    private ArticleRepository articleRepository;
+
+    @GetMapping("/articles/new")
+    public String newArticleForm(){
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form){
+        // System.out.println(form.toString());
+        log.info(form.toString());
+
+        // 1. DTO를 엔티티로 변환
+        Article article = form.toEntity();
+        // System.out.println(article.toString());
+        log.info(article.toString());
+
+        // 2. 리포지터리로 엔티티를 DB에 저장
+        Article saved = articleRepository.save(article);
+        // System.out.println(saved.toString());
+        log.info(saved.toString());
+        return "articles/new";
+    }
+}
