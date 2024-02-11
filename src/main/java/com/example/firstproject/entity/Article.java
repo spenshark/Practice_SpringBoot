2103382934f8ef23ae84b4ad200bba08532a9afd
Sangjun Lee <xzx01299@gachon.ac.kr>
2024-02-11 13:57:38 +0900

    CRUD test

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -5,11 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 @Entity
+@Getter
 public class Article {
 
     @Id
@@ -19,5 +23,4 @@ public class Article {
     private String title;
     @Column
     private String content;
-
 }
